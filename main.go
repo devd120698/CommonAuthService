9a5e19a030474f2a1f6bfc3c7241093867c670f9
devd120698 <divyanshdubey538@gmail.com>
2023-10-21 20:19:14 +0530

    Echo Framework base template

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
+import "github.com/labstack/echo"
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
-	})
+	//http.HandleFunc("/signIn", func(w http.ResponseWriter, r *http.Request) {
+	//	fmt.Fprintf(w, "Hello, World!")
+	//})
+	//
+	//http.ListenAndServe(":8080", nil)
+	e := echo.New()
+	e.Logger.Fatal(e.Start(":8000"))
 
-	http.ListenAndServe(":8080", nil)
 }
