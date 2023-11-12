689f291f7abe8dd60044ac531c0ee1c86615b278
devd120698 <divyanshdubey538@gmail.com>
2023-11-12 19:14:47 +0530

    Add Get User API

--- main.go
+++ main.go
@@ -23,10 +23,7 @@ func main() {
 
 	e.POST("/createUser", createUser)
 	e.GET("/getUser", getUser)
-	/*
-		Login API
-		Logout API
-	*/
+
 	go e.Logger.Fatal(e.Start(":9008"))
 
 	// Graceful Shutdown
